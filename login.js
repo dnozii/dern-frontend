8c0df4e5050cdba2f1b4067e2cc425e5f7a6b662
dnozii <adilnoza06@gmail.com>
2025-05-29 17:34:05 +0500

    Last update

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
       try {
         const res = await fetch("https://dern-backend.onrender.com/api/users/login", {
-          method: "POST",
+              method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password })
         });
